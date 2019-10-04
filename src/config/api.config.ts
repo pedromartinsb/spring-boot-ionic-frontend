91e81218455e680cd7ebb5bcb0f3d1359eb3325b
Pedro Martins <engpedrocampos@gmail.com>
2019-10-04 09:24:02 -0300

    Criando um FormGroup para controlar o formulario

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    //baseUrl: "http://localhost:8080",
+    baseUrl: "http://192.168.0.23:8080",
     bucketBaseUrl: "https://sistema-pedidos-aquarela.s3-sa-east-1.amazonaws.com"
 }
