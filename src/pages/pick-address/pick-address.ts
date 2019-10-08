23e185b317e5f7c187452f93fc159039eea0cba5
Pedro Martins <engpedrocampos@gmail.com>
2019-10-08 10:45:04 -0300

    Tela de escolha de endereço

---
+++ src/pages/pick-address/pick-address.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EnderecoDTO } from '../../models/endereco.dto';
+
+@IonicPage()
+@Component({
+  selector: 'page-pick-address',
+  templateUrl: 'pick-address.html',
+})
+export class PickAddressPage {
+
+  items: EnderecoDTO[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.items = [
+      
+    ]
+  }
+
+}
